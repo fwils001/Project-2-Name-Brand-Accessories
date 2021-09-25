2c1bd98f9cfae5b7a221de745bf5a486018ef3e4
fwils001 <fwils001@ucr.edu>
2021-09-25 08:31:48 -0700

    more work done on routes

--- models/seed.js
+++ models/seed.js
@@ -5,5 +5,15 @@ module.exports = [
         color: "purple/pink",
         description: "Marc Jacobs Handbag. Excellent condition. Like new.",
         price: 89
-    }
-] 
+    },
+    {
+        name: "Mark Cross Handbag",
+        img: "https://imgur.com/GZh9QZw.jpg",
+        color: "white",
+        description: "Vintage Mark Cross Handbag. Excellent condition.",
+        price: 45
+    } 
+    
+      
+]
+        
--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
 
-const methodOverride = require('method-override')
+   const express = require('express')
+   const app = express()
+   const port = 3000
+   const methodOverride = require('method-override')
+   
+   app.use(methodOverride('_method'))
+   
+   app.use(express.static('public'))
 
-app.use(methodOverride('_method'))
-
-
-app.use(express.static('public'))
-
-app.use(express.json())
-
-app.use(express.urlencoded({extended: false}))
-
-const items = require('./models/seed.js')
-console.log(items)
-
-
-app.get('/', (req, res) => {
-  res.send(items)
-    })
+   app.use(express.json())
+   
+   app.use(express.urlencoded({extended: false}))
+   
+   const items = require('./models/seed.js')
+   console.log(items)
+   
+   app.get('/', (req, res) => {
+    res.send(items)
+      }) 
+    
     app.get('/items', (req, res) => {
       res.render('index.ejs', {
           items: items
@@ -27,68 +26,71 @@ app.get('/', (req, res) => {
   })
   
   app.get('/items/new',(req, res) => {
-      res.render('new.ejs')
-    })
+    res.render('new.ejs')
+  })
   
   app.get('/items/:id', (req, res) => {
-      // console.log(items[req.params.id])
-      console.log(items)
-      res.render("show.ejs", {
-          items: items[req.params.id],
-          itemId: req.params.id
-  
-      })
-  })
+    // console.log(items[req.params.id])
+    console.log(items)
+    res.render("show.ejs", {
+        items: items[req.params.id],
+        itemId: req.params.id
+
+    })
+})
   
   app.post('/items', (req, res) =>{
-  
-      const itemToAdd = {
-          name: req.body.name,
-          img: req.body.img,
-          description: req.body.description,
-          price: req.body.price
-      }
-  
-      items.push(itemToAdd)
-  
-      res.redirect('/items')
-  })
+
+    const itemToAdd = {
+        name: req.body.name,
+        img: req.body.img,
+        description: req.body.description,
+        price: req.body.price
+    }
+     items.push(itemToAdd)
+
+     res.redirect('/items')
+ })
   
   app.delete('/items/:id', (req, res)=> {
-  
-     const indexOfItemToDelete = items[req.params.id]
-  
-      items.splice(indexOfItemToDelete, 1)
-  
-      console.log(indexOfItemToDelete)
-  
-      res.redirect('/items')
-    })
-  
-  
-  app.get('/items/:id/edit', (req, res) => {
-      console.log(req.params.id)
-      const itemToEdit = items[req.params.id]
-      res.render('edit.ejs', {
-        name: req.params.id.name,
-        img: req.params.id.img,
-        items: itemToEdit,
-        indexOfItemToDelete: req.params.id
-       })
-    })
-  
-    app.put('/items/:id', (req, res) => {
-      const updatedItem = {
-        name: req.body.name
-  
-      }
-      items[req.params.id] = updatedItem
-  
-  
-      res.redirect('/items')
-    })
+
+    const indexOfItemToDelete = items[req.params.id]
+
+     items.splice(indexOfItemToDelete, 1)
+
+     console.log(indexOfItemToDelete)
+
+     res.redirect('/items')
+   })
+
+
+ app.get('/items/:id/edit', (req, res) => {
+     console.log(req.params.id)
+     const itemToEdit = items[req.params.id]
+     res.render('edit.ejs', {
+       name: req.params.id.name,
+       img: req.params.id.img,
+       items: itemToEdit,
+       indexOfItemToDelete: req.params.id
+      })
+   })
+
+  app.get('/items/welcome', (req, res) => {
+    res.render('create.ejs')
+  })
+
+   app.put('/items/:id', (req, res) => {
+     const updatedItem = {
+       name: req.body.name
+
+     }
+     items[req.params.id] = updatedItem
+
+
+     res.redirect('/items')
+   })
 
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000")
-})
+ console.log("Server is running on port 3000")
+}) 
